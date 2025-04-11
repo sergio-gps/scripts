b52133f9648615ca0880350f28396e37ef9348c4
Sergio <26248969+sergio-gps@users.noreply.github.com>
2025-04-11 20:01:19 +0200

    Create thunderbird_beta.sh
    Script para descargar Thunderbird Beta desde la web de Mozilla

---
+++ thunderbird_beta.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script para descargar Thunderbird Beta desde la web de Mozilla
+#
+
+locFilePath="$HOME/Descargas/thunderbird_beta.tar.xz"
+urlLatest="https://download.mozilla.org/?product=thunderbird-beta-latest-SSL&os=linux64&lang=es-ES"
+
+wget -nv --show-progress -O "${locFilePath}" "${urlLatest}"
+
+sudo tar -xJf "${locFilePath}" -C /opt
+rm "${locFilePath}"
+
+sudo ln -s /opt/thunderbird/thunderbird /usr/local/bin/thunderbird
+
+sudo wget --no-clobber https://raw.githubusercontent.com/mozilla/sumo-kb/main/installing-thunderbird-linux/thunderbird.desktop -P /usr/local/share/applications
