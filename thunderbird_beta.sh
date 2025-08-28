ea0a0ad494309a816df7091bf697e7b6e2af8ed5
Sergio <26248969+sergio-gps@users.noreply.github.com>
2025-08-28 12:59:28 +0200

    Update thunderbird_beta.sh
    script actualizado

--- thunderbird_beta.sh
+++ thunderbird_beta.sh
@@ -5,6 +5,7 @@
 
 locFilePath="$HOME/Descargas/thunderbird_beta.tar.xz"
 urlLatest="https://download.mozilla.org/?product=thunderbird-beta-latest-SSL&os=linux64&lang=es-ES"
+icon="/usr/local/share/applications"
 
 wget -nv --show-progress -O "${locFilePath}" "${urlLatest}"
 
@@ -13,4 +14,6 @@ rm "${locFilePath}"
 
 sudo ln -s /opt/thunderbird/thunderbird /usr/local/bin/thunderbird
 
-sudo wget --no-clobber https://raw.githubusercontent.com/mozilla/sumo-kb/main/installing-thunderbird-linux/thunderbird.desktop -P /usr/share/applications/thunderbird-beta.desktop
+sudo wget --no-clobber https://raw.githubusercontent.com/mozilla/sumo-kb/main/installing-thunderbird-linux/thunderbird.desktop -P "$icon"
+
+sudo mv "$icon"/thunderbird.desktop "$icon"/thunderbird-beta.desktop
